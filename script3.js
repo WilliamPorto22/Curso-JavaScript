c3dd447ff572f5752325bddcb9ebb8686b40edc6
WilliamPorto22 <williamporto0@gmail.com>
2022-09-11 09:11:33 -0300

    Atualizacao exemplo 4

--- script3.js
+++ script3.js
@@ -2,4 +2,9 @@ let n1 = Number(window.prompt('Digite um Numero:'))  // (Number.parseInt) ou so 
 let n2 = Number(window.prompt('Digite outro Numero:'))
 let s = n1 + n2 
 
-window.alert('A soma dos valores é ' + s) // Lembrando que o simbolo do mais e igual a juntar (concatenar) nao a somar
+window.alert(`A soma entre ${n1} e ${n2}  é igual a ${s}` ) // Lembrando que o simbolo do mais e igual a juntar (concatenar) nao a somar
+
+
+
+// este exemplo abaixo e e a segunda opçao para concatenar 
+// window.alert('A soma dos valores é ' + s)
