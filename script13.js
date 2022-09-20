0bb4ad72aab6c8ab4adeb5b00b6042b1d0f4bf4c
WilliamPorto22 <williamporto0@gmail.com>
2022-09-20 15:24:21 -0300

    Atualizacao

--- script13.js
+++ script13.js
@@ -3,3 +3,26 @@ function comerPizza() {
         comerFatia()
     }
 }
+
+let c = 1
+while(c <= 10){
+    console.log('Tudo bem?')
+    c++ // Lembrando que ++ Significa  c = c + 1   ou Acresentar mais 1 na variavel
+}
+
+
+// Exemplos Condicoes 
+
+// 5 > 2 = 5 é maior que 2 
+
+// 7 < 4 = 7 é menor do que 4
+
+// 8 >= 8 = 8 é maior ou igual a 8 
+
+// 9 <= 7 = 9 é menor ou igual a 7
+
+// 5 == 5 = 5 e igual a 5 
+
+// 4 != 4 = 4 é diferente de 4
+
+// 2 = 1 = 2 recebe 1 
---
+++ script14.js
@@ -0,0 +1,26 @@
+// FOR 
+
+// Exemplo abaixo e um while que é parecido com o for 
+
+let c = 1
+
+while (c <= 10){
+    console.log(c)
+    c++
+}
+
+
+// Fazer o mesmo exercicio so que agora com o FOR!! 
+
+// For é o que é mais utilizado 
+
+for ( let a = 20 ; a <= 30 ; a++ ){
+    console.log(a)
+}
+
+
+// exemplo 2 
+
+for ( let b = 10000000000 ; b <= 11000000000 ; b+b ){
+    console.log(`William Porto Tera entre R$${b} ate os 33 Anos!`)
+}
