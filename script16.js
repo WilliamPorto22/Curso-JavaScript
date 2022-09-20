9db1dbdbaeeaa1d1108f2ff2fcab5ce4de8650fd
WilliamPorto22 <williamporto0@gmail.com>
2022-09-20 17:24:11 -0300

    Atualizacao Function

---
+++ script16.js
@@ -0,0 +1,20 @@
+// Function Exemplos
+
+// Desobrir se o numero e Par ou Impar
+
+let resultado = parimp()
+let numeroinfirmado = 130
+
+function parimp(numero){
+    if(numero%2==0){
+        return 'Par'
+    }else{
+        return 'Impar'
+    }
+}
+
+
+console.log(` O Numero ${numeroinfirmado} é ${resultado}!!`)
+
+
+// Acima feita por Willim 
