3c8619fa6e54557ca989099c756473b740463eac
WilliamPorto22 <williamporto0@gmail.com>
2022-09-20 12:14:11 -0300

    Atualizacao

--- script11.js
+++ script11.js
@@ -1,5 +1,7 @@
 // Condicao para Confirmar o horario e reagir
-let hora = 20
+
+let agora = new Date()
+let hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora<=12){
     console.log('Bom Dia!!')
